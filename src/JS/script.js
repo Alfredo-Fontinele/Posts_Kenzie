86269d9feba345b816b3642c6e994257eec14857
Alfredo <alfredofontinele08@gmail.com>
2022-06-10 19:44:42 -0300

    commit

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,18 +1,19 @@
+const navbar = document.querySelector(".header_right");
+const button = document.querySelector(".menu_mobile");
+
+button.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+})
+
 function mudarTema() {
-    let main = document.querySelector("main");
-    let header = document.querySelector("header");
     let body = document.querySelector("body");
     if (body.classList.contains("bg1")) {
         body.classList.remove("bg1");
         body.classList.add("bg2");
-        main.style.backgroundColor = "rgba(20,30,40, 0.8)";
-        header.style.backgroundColor = "rgb(20,30,40)";
         header.style.color = "#FFF";
     } else {
         body.classList.remove("bg2");
         body.classList.add("bg1");
-        header.style.backgroundColor = "#FFF";
         header.style.color = "#000";
-        main.style.backgroundColor = "#FFF";
     }
 }
